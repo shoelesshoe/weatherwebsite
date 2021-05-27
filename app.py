29bf77685bb73d5423d575877b24d5d19757a0a7
shoe <70523543+shoelesshoe@users.noreply.github.com>
2021-05-27 23:55:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
-print("hello world")
+from datetime import datetime
+import requests
+
+app = Flask(__name__)
+    
+
+@app.route('/')
+def home():
+    year = datetime.now().year
+
+    month = datetime.now().month
+    month = str(month).zfill(2)  # checks whether month is single digit or not and fills it with a 0 in front if it is
+
+    date = datetime.now().day
+    date = str(date).zfill(2)  # # checks whether date is single digit or not and fills it with a 0 in front if it is
+
+    response = requests.get(f"https://api.data.gov.sg/v1/environment/24-hour-weather-forecast?date={year}-{month}-{date}").json()
+
+    # kwargs = {
+        # "forecast": response["items"]["general"]["forecast"]
+    # }
+    
+    return render_template("home.html")
+
+
+
+
+# TODO: add alerts (pop-up) when some area is raining
