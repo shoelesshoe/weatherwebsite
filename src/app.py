8551ffa94dbb25c8731ca5a9676cae6dcf446b2a
shoelesshoe <randal0913@gmail.com>
2021-06-26 13:41:38 +0800

    updated app.py, style.css and index.html

--- src/app.py
+++ src/app.py
@@ -5,34 +5,34 @@ import requests
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def home():
-    year = datetime.now().year
-
-    month = datetime.now().month
-    month = str(month).zfill(2)  # checks whether month is single digit or not and fills it with a 0 in front if it is
-
-    date = datetime.now().day
-    date = str(date).zfill(2)
-
-    hours = datetime.now().hour
-    hours = str(hours).zfill(2)
-
-    minutes = datetime.now().minute
-    minutes = str(minutes).zfill(2)
-
-    seconds = datetime.now().second
-    seconds = str(seconds).zfill(2)
-
-    next_2_hour_forecast = requests.get(f"https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time={year}-{month}-{date}T{hours}-{minutes}-{seconds}").json()
+    ## obtain data
+    next_2_hours_forecast = requests.get("https://api.data.gov.sg/v1/environment/2-hour-weather-forecast").json()
     # this provides info for each location in sg (forecast only)
-    today_forecast = requests.get(f"https://api.data.gov.sg/v1/environment/24-hour-weather-forecast?date_time={year}-{month}-{date}T{hours}-{minutes}-{seconds}").json()
+    today_forecast = requests.get("https://api.data.gov.sg/v1/environment/24-hour-weather-forecast").json()
     # this provides info for the whole day of sg (forecast, humidity, temp, wind direction and speed) and forecasts for each region of sg every 6h starting from 00:00
-    next_4_days_forecast = requests.get(f"https://api.data.gov.sg/v1/environment/4-day-weather-forecast?date_time={year}-{month}-{date}T{hours}-{minutes}-{seconds}").json()
+    next_4_days_forecast = requests.get("https://api.data.gov.sg/v1/environment/4-day-weather-forecast").json()
     # this provides info for the NEXT 4 days for the whole day of sg (forecast, humidity, temp, wind direction and speed)
 
-    # kwargs = {
-        # "forecast": response["items"]["general"]["forecast"]
-    # }
+    ## today_day
+    today_day_name = datetime.now().strftime("%A")  # today's day name
+
+    ## today_month_date
+    month = datetime.now().month
+    datetime_obj = datetime.strptime(str(month), "%m")
+    today_month_name = datetime_obj.strftime("%b")  # in short form
+
+    today_day = datetime.now().day
+
+    today_month_date = f"{today_month_name} {today_day}"
+
+    kwargs = {
+        "today_day": today_day,
+        "today_month_date": today_month_date,
+        "today_high_temp": today_forecast['items'][0]['general']['temperature']['high'],
+        "today_low_temp": today_forecast['items'][0]['general']['temperature']['low'],
+        
+    }
     
-    return render_template("home.html")
+    return render_template("home.html", **kwargs)
